ad457a932e62007c0a41426ec552201c3eb9e022
Julius Gonsior <julius@gaenseri.ch>
2017-04-21 00:04:25 +0200

    solved ex3 again^^

--- ex3.hs
+++ ex3.hs
@@ -1,5 +1,22 @@
-data MyBoolean = Yes | No
+data MyBoolean = Yes | No deriving Show
 
 if' :: MyBoolean -> t -> t -> t
 if' Yes a b = a
 if' No a b = b
+
+
+not' :: MyBoolean -> MyBoolean
+not' Yes = No
+not' No = Yes
+
+or' :: MyBoolean -> MyBoolean -> MyBoolean
+or' Yes Yes = Yes
+or' Yes No = Yes
+or' No Yes = Yes
+or' No No = No
+
+and' :: MyBoolean -> MyBoolean -> MyBoolean
+and' Yes Yes = Yes
+and' Yes No = No
+and' No Yes = No
+and' No No = No
