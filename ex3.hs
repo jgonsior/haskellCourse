ea76a0c52320629dbd03051591cbe393328d263b
Julius Gonsior <julius@gaenseri.ch>
2017-04-21 14:34:01 +0200

    added the long form of the case construct

--- ex3.hs
+++ ex3.hs
@@ -2,21 +2,26 @@ data MyBoolean = Yes | No deriving Show
 
 if' :: MyBoolean -> t -> t -> t
 if' Yes a b = a
-if' No a b = b
+if' No a b  = b
 
 
 not' :: MyBoolean -> MyBoolean
 not' Yes = No
-not' No = Yes
+not' No  = Yes
 
 or' :: MyBoolean -> MyBoolean -> MyBoolean
 or' Yes Yes = Yes
-or' Yes No = Yes
-or' No Yes = Yes
-or' No No = No
+or' Yes No  = Yes
+or' No Yes  = Yes
+or' No No   = No
 
 and' :: MyBoolean -> MyBoolean -> MyBoolean
 and' Yes Yes = Yes
-and' Yes No = No
-and' No Yes = No
-and' No No = No
+and' Yes No  = No
+and' No Yes  = No
+and' No No  = No
+
+not'' t =
+  case t of
+    Yes -> No
+    No  -> Yes
