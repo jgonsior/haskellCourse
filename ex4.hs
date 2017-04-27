de007b24e5b0c5464f1cdc989b6755db740597b9
Julius Gonsior <julius@gaenseri.ch>
2017-04-27 16:04:03 +0200

    added renderDocument

--- ex4.hs
+++ ex4.hs
@@ -1,6 +1,13 @@
-import           Data.List
+--module HtmlLibrary (renderHtml(Html)) where
+
+import Data.List
 
 newtype Html = Html String deriving Show
+data Doctype = Html4 | Html5 | XHtml deriving Show -- Html4 sollte Html heißen
+data Document = Document { doctype :: Doctype
+                         , headSection :: Html
+                         , bodySection :: Html
+                         } deriving (Show)
 
 createHtml :: String -> Html
 createHtml string = Html string
@@ -22,3 +29,22 @@ makeI (Html a) = createHtml ("<i>" ++ a ++ "</i>")
 
 makeSpan :: Html -> Html
 makeSpan (Html a) = createHtml ("<span>" ++ a ++ "</span>")
+
+makeHead :: Html -> Html
+makeHead (Html a) = createHtml ("<head>" ++ a ++ "</head>")
+
+makeBody :: Html -> Html
+makeBody (Html a) = createHtml ("<body>" ++ a ++ "</body>")
+
+
+makeDocument :: Doctype -> Document
+makeDocument Html4 = Document Html4 (makeHead (Html "")) (makeBody (Html ""))
+--renderDocument Html5 =
+--renderDocument XHtml =
+
+-- renderDocument :: Document -> String
+renderDocument Document{
+                       doctype = Html4
+                       , headSection=headSection
+                       , bodySection=bodySection
+                       } = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">" ++ renderHtml(headSection) ++ renderHtml(bodySection)
