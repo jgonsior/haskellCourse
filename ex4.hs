0787eaef8133b0621e774d10ed0c1bed50a99230
Julius Gonsior <julius@gaenseri.ch>
2017-05-05 19:35:27 +0200

    fixed renderHtml in ex4

--- ex4.hs
+++ ex4.hs
@@ -1,10 +1,10 @@
 --module HtmlLibrary (renderHtml(Html)) where
 
-import           Data.List
+import Data.List
 
-data Html = String | Container { containerTag        :: String
-                               , containerAttributes :: [(String,String)]
-                               , containerChildren   :: [Html]
+data Html = Text String | Container { tag        :: String
+                               , attributes :: [(String,String)]
+                               , children   :: [Html]
                                } deriving (Show)
 
 data Doctype = Html4 | Html5 | XHtml deriving Show -- Html4 sollte Html heißen
@@ -13,42 +13,52 @@ data Document = Document { doctype     :: Doctype
                          , bodySection :: Html
                          } deriving (Show)
 
-createHtml :: String -> Html
-createHtml string = Container{containerTag="", containerChildren=[string]}
+createHtml :: String -> [(String, String)] -> Html
+createHtml string  attributes = Container {tag="", attributes= attributes, children=[Text string]}
+
+renderAttributes :: [(String, String)] -> String
+renderAttributes [] = " "
+renderAttributes ((name, value):attributes) = name ++ "=\"" ++ value ++ "\""
+                                              ++ (renderAttributes attributes)
 
 renderHtml :: Html -> String
-renderHtml Container{
-                    containerTag=containerTag
-                    , containerAttributes=(containerAttribute:containerAttributes)
-                    , containerChildren=(containerChild:containerChildren)
-                    } = "<" ++ containerTag ++ ">" ++ (renderHtml Container containerTag containerAttributes containerChildren) ++ "</" ++ containerTag ++ ">"
+renderHtml (Text text) = text
+renderHtml Container{children=[]} = ""
+renderHtml Container{tag=tag
+                     , attributes=attributes
+                     , children=(child:children)
+                     } = "<" ++ tag
+                         ++ (renderAttributes attributes) ++ ">"
+                         ++ (renderHtml child)
+                         ++ "</" ++ tag ++ ">"
 
 makeTextNode :: String -> Html
 makeTextNode a =
   if isInfixOf ['<', '>', '=', '&'] a
     then error "Not valid HTML code"
     else createHtml a
-
+{-
 makeDiv :: Html -> Html
-makeDiv a = Container{containerTag="div", containerAttributes="", containerChildren = [a]}
+makeDiv a = Container{tag="div", attributes="", children = [a]}
 
 makeI :: Html -> Html
-makeI a = Container{containerTag="i", containerAttributes="", containerChildren = [a]}
+makeI a = Container{tag="i", attributes="", children = [a]}
 
 makeSpan :: Html -> Html
-makeSpan a = Container{containerTag="span", containerAttributes="", containerChildren = [a]}
+makeSpan a = Container{tag="span", attributes="", children = [a]}
 
 makeHead :: Html -> Html
-makeHead a = Container{containerTag="head", containerAttributes="", containerChildren = [a]}
+makeHead a = Container{tag="head", attributes="", children = [a]}
 
 makeBody :: Html -> Html
-makeBody a = Container{containerTag="body", containerAttributes="", containerChildren = [a]}
+makeBody a = Container{tag="body", attributes="", children = [a]}
 
 
 makeDocument :: Doctype -> Document
 makeDocument Html4 = Document Html4 (makeHead ("")) (makeBody (""))
 makeDocument Html5 = Document Html5 (makeHead ("")) (makeBody (""))
 makeDocument XHtml = Document XHtml (makeHead ("")) (makeBody (""))
+-}
 --renderDocument Html5 =
 --renderDocument XHtml =
 {-
